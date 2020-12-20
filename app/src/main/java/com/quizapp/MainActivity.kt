51b79545195d01332e9cbf5ca7111bb35e569a27
Liubomyr Izdepskyi <lizdepskij@gmail.com>
2020-12-20 23:52:35 +0200

    Question UI Part 2

--- app/src/main/java/com/quizapp/MainActivity.kt
+++ app/src/main/java/com/quizapp/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.quizapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import android.widget.Toast.makeText
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,5 +24,15 @@ class MainActivity : AppCompatActivity() {
         // To hide the status bar.
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
         // END
+
+        btn_start.setOnClickListener {
+            if (et_name.text.toString().isEmpty()) {
+                makeText(this, "Please enter your name", Toast.LENGTH_SHORT).show()
+            } else {
+                val intent = Intent(this, QuizQuestionActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/quizapp/Question.kt
@@ -0,0 +1,13 @@
+package com.quizapp
+
+data class Question (
+    val id : Int,
+    val Question: String,
+    val image: Int,
+    val optionOne: String,
+    val optionTwo: String,
+    val optionThree: String,
+    val optionFour: String,
+    val correctAnswer: Int
+
+)
